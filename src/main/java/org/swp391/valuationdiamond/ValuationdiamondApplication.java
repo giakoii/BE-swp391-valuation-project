f2a090e1176e151dbbb37472727e8ab545c6c4ac
giakoii <khoipgse172073@fpt.edu.vn>
2024-06-29 19:52:21 +0700

    thêm file docker

--- src/main/java/org/swp391/valuationdiamond/ValuationdiamondApplication.java
+++ src/main/java/org/swp391/valuationdiamond/ValuationdiamondApplication.java
@@ -8,6 +8,7 @@ public class ValuationdiamondApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValuationdiamondApplication.class, args);
+		System.out.println("Success!");
 	}
 
 }
